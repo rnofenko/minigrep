d25a9c413f24c9ae937f9ec0853cee4baf067f45
Zlata Nofenko <rnofenko@gmail.com>
2023-04-05 15:00:25 -0700

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, String> {
-        let args_len = args.len() - 1;
-        if args_len < 2 {
-            let msg = format!("{args_len} arguments instead of 2");
-            return Err(msg);
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item=String>) -> Result<Config, &'static str> {
+        args.next();
 
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file_path string")
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
@@ -47,13 +49,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Hello, world!");
     dbg!(&args);
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
